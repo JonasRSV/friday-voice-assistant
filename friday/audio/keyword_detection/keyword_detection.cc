69fd9d0ed2f066b814da35d1512208da99679c01
Jonas Valfridsson <jonas@valfridsson.net>
2020-10-18 21:41:00 +0200

    debug

--- friday/audio/keyword_detection/keyword_detection.cc
+++ friday/audio/keyword_detection/keyword_detection.cc
@@ -107,8 +107,8 @@ std::string prediction() {
     std::cout << "Predicting" << std::endl;
 
 
-    //pred = goldfish::predict(predict_frame, frame_size);
-    //prediction = argmax(pred.probabilities.data());
+    pred = goldfish::predict(predict_frame, frame_size);
+    prediction = argmax(pred.probabilities.data());
 
     //LOG(DEBUG) << TAG("keyword_detection") << AixLog::Color::YELLOW
     //           << "probabilities: " << pred.probabilities
